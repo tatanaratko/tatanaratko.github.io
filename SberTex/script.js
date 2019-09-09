cfa7ff7941d440085cb6dd752d77e774dd1ba6b7
Tatyana Ratko <tatanaratko@gmail.com>
2019-09-09 14:51:41 +0600

    Comments for script

---
+++ SberTex/script.js
@@ -0,0 +1,40 @@
+var Date= {
+    "ProfileId": "12w562qrx",
+    "PersonInfo": {
+        "Name": "Василий Иванов",
+        "BirthDate": "12-09-1990",
+        "Citizenship": "Russian Federation"
+    },
+    "CardInfo": [
+        {
+            "CardNumber": "1234890456793333",
+            "CardName": "VISA CLASSIC",
+            "ExpDate": "30-02-2019"
+        },
+        {
+            "CardNumber": "1234000145292133",
+            "CardName": "MASTERCARD GOLD",
+            "ExpDate": "21-05-2020"
+        },
+        {
+            "CardNumber": "1234000145293333",
+            "CardName": "MIR КЛАССИЧЕСКАЯ",
+            "ExpDate": "02-12-2019"
+        }
+    ]
+  }
+
+
+  //Создать массив "oldCards" для карть с истекшим сроком
+  //Получить доступ к ключу "CardInfo"
+  //Зайти внутрь этого массива
+  //Получить доступ к первому, второму...двадцатому объекту массива
+  //В первом объекте получить значение ключа "ExpDate"
+  //Если это значение < , чем значение поля Date, то:  
+  //В первом объекте получить значение ключа "CardNumber"
+  //Отсчитать у значения последние 4 цифры
+//Записать это число в массив "oldCards" 
+//Перебрать таким образом все элементы
+function getCardsArrayExpDate(json, date) {
+	get.Date.CardInfo
+}
