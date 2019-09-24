f06aafd7e1d4ad17c95ae5579bbdfdedd2c17c3c
Tatyana Ratko <tatanaratko@gmail.com>
2019-09-24 15:13:16 +0600

    JSON start working

---
+++ JSON&AJAX/js/data-html.js
@@ -0,0 +1,10 @@
+﻿var xhr= new XMLHttpRequest();
+
+xhr.onload = function () {
+  document.getElementById('content').innerHTML=xhr.responseText;
+};
+
+xhr.open('GET', 'data/data.html', true);
+xhr.send(null);
+
+
