64553f46b1e9ea0a15fce7518a35d7f706c4171f
Ратько Татьяна Алексеевна <Ratko2-TA@int.zsb>
2020-01-28 14:04:11 +0600

    return data

--- python_operations_task/task.py
+++ python_operations_task/task.py
@@ -1,6 +1,7 @@
 import json
 import datetime
 
+
 def catch_data():
     with open('operations.json', 'r', encoding='utf-8') as f:
         json_data = json.load(f)
@@ -11,19 +12,27 @@ def sort_dates(data):
     new_data = []
     for user_data in data:
         if user_data == {}:
-            print('no data')
+            print('no data here')
         else:
             new_data.append(user_data)
-    sort = sorted(new_data ,key=lambda user_data: user_data['date'])
+    sort = sorted(new_data, key=lambda user_data: user_data['date'])
     return sort
 
 
 def five_dates(sorted_data):
     result_datas = sorted_data[-5:]
     result_datas.reverse()
+    dates_to_print = []
     for d in result_datas:
-        print(d['date'])
-    return result_datas
+        if d['date'] in result_datas and d['description'] in result_datas and d['from'] in result_datas and ['to'] in result_datas:
+            return_date = d['date']
+            return_des = d['description']
+            return_to = d['to']
+            return_from = d['from']
+            print(f'{return_date} {return_des}\n'
+                  f'{return_from} -> {return_to}\n'
+                  'сумма первода и валюта\n')
+    return
 
 cd = catch_data()
 sd = sort_dates(cd)
