ac13967879734e4a6bbd208488bfa4cc5726302a
Mikhail Semikolenov <mihawww@yandex.ru>
2020-01-28 22:52:54 +0600

    Masking

--- python_operations_task/task.py
+++ python_operations_task/task.py
@@ -1,7 +1,6 @@
 import json
 import datetime
 
-
 def catch_data():
     with open('operations.json', 'r', encoding='utf-8') as f:
         json_data = json.load(f)
@@ -12,7 +11,7 @@ def sort_dates(data):
     new_data = []
     for user_data in data:
         if user_data == {}:
-            print('no data here')
+            pass
         else:
             new_data.append(user_data)
     sort = sorted(new_data, key=lambda user_data: user_data['date'])
@@ -24,15 +23,47 @@ def five_dates(sorted_data):
     result_datas.reverse()
     dates_to_print = []
     for d in result_datas:
-        if d['date'] in result_datas and d['description'] in result_datas and d['from'] in result_datas and ['to'] in result_datas:
-            return_date = d['date']
-            return_des = d['description']
-            return_to = d['to']
-            return_from = d['from']
-            print(f'{return_date} {return_des}\n'
-                  f'{return_from} -> {return_to}\n'
-                  'сумма первода и валюта\n')
+        from_account='anon'
+        date = d['date']
+        date_format=datetime.datetime.strptime(date, '%Y-%m-%dT%H:%M:%S.%f')
+        date_str=date_format.strftime('%d.%m.%Y')
+        to = d['to']
+        to_number=extract_number(to)
+        to_masked = 'incorrect number (to)'
+        if len(to_number) ==16:
+            to_masked=mask_card_number(to_number)
+        elif len(to_number) == 20:
+            to_masked=mask_acc_number(to_number)
+        des = d['description']
+        if 'from' in d: 
+            from_account = d['from']
+
+        amount = d['operationAmount']['amount']
+        currency = d['operationAmount']['currency']['name']
+        print('{date} {des}\n{from_acc} -> {to}\n{amount} {currency}\n'.format(
+            date=date_str,
+            des=des,
+            to=to_masked,
+            from_acc=from_account,
+            amount=amount,
+            currency=currency
+        ))
     return
+
+def extract_number(string):
+    splited = string.split()
+    number = splited[-1]
+    if number.isdigit():
+        return number
+    return 'not a number'
+
+def mask_acc_number(acc_number):
+    masked = '**' + acc_number[-4:]    
+    return masked
+
+def mask_card_number(card_number):
+    masked = card_number[:4]+ ' '+card_number[5:7]+ '** **** '+ card_number[-4:]  
+    return masked
 
 cd = catch_data()
 sd = sort_dates(cd)
