bacc195fe511054aa35dd529b330edc6add9eef7
Mikhail Semikolenov <mihawww@yandex.ru>
2020-02-08 14:19:58 +0600

    Refactoring

--- python_operations_task/task.py
+++ python_operations_task/task.py
@@ -1,6 +1,9 @@
 import json
 import datetime
 
+CARD_NUMBER_LEN = 16
+ACCOUNT_NUMBER_LEN = 20
+
 def catch_data():
     with open('operations.json', 'r', encoding='utf-8') as f:
         json_data = json.load(f)
@@ -21,51 +24,37 @@ def sort_dates(data):
     sort = sorted(data, key=lambda user_data: user_data['date'])   
     return sort
 
-
 def get_results(sorted_data):
-    result_datas = sorted_data[-5:]
-    result_datas.reverse()
-    dates_to_print = []
-    for d in result_datas:
+    five_datas_result = get_five_datas(sorted_data)
+
+    for d in five_datas_result:
         from_account = '****'
+
         if 'from' in d:
             from_account = d['from']
-        date = d['date']
-        date_format=datetime.datetime.strptime(date, '%Y-%m-%dT%H:%M:%S.%f')
-        date_str=date_format.strftime('%d.%m.%Y')
+        
         to = d['to']
-        to_number = extract_number(to)
         to_acc = extract_acc_type(to)
+        to_number = extract_number(to)
+        to_masked = get_masked_number(to_number)
+        
         from_acc = extract_acc_type(from_account)
         from_number = extract_number(from_account)
-        to_masked = ''
-
-        if len(to_number) ==16:
-            to_masked = mask_card_number(to_number)
-        elif len(to_number) == 20:
-            to_masked = mask_acc_number(to_number)
-        from_masked = ''
-        if len(from_number) ==16:
-            from_masked = mask_card_number(from_number)
-        elif len(from_number) == 20:
-            from_masked = mask_acc_number(from_number)
+        from_masked = get_masked_number(from_number)
             
-        des = d['description']
+        print_result(d, to_acc, to_masked, from_acc, from_masked)
+        
 
+def get_five_datas (data):
+    result_datas = data[-5:]
+    result_datas.reverse()
+    return result_datas
 
-
-        amount = d['operationAmount']['amount']
-        currency = d['operationAmount']['currency']['name']
-        print('{date} {des}\n{from_type} {from_acc} -> {to_account} {to}\n{amount} {currency}\n'.format(
-            date=date_str,
-            des=des,
-            to_account=to_acc,
-            to=to_masked,
-            from_type=from_acc,
-            from_acc=from_masked,
-            amount=amount,
-            currency=currency
-        ))
+def format_date(date_str):
+    date_format=datetime.datetime.strptime(date_str, '%Y-%m-%dT%H:%M:%S.%f')
+    result=date_format.strftime('%d.%m.%Y')
+    return result
+    
 
 def extract_number(string):
     splited = string.split()
@@ -73,6 +62,15 @@ def extract_number(string):
     if number.isdigit():
         return number
     return 'not a number'
+
+def get_masked_number(number):
+    masked = ''
+    if len(number) == CARD_NUMBER_LEN:
+        masked = mask_card_number(number)
+    elif len(number) == ACCOUNT_NUMBER_LEN:
+        masked = mask_acc_number(number)
+
+    return masked
 
 def extract_acc_type(string):
     splited = string.split()
@@ -90,6 +88,22 @@ def mask_acc_number(acc_number):
 def mask_card_number(card_number):
     masked = card_number[:4]+ ' '+card_number[5:7]+ '** **** ' + card_number[-4:]
     return masked
+
+def print_result(data_to_print, to_acc, to_masked, from_acc, from_masked):
+    des = data_to_print['description']
+    amount = data_to_print['operationAmount']['amount']
+    currency = data_to_print['operationAmount']['currency']['name']
+    date_str = format_date(data_to_print['date'])
+    print('{date} {des}\n{from_type} {from_acc} -> {to_account} {to}\n{amount} {currency}\n'.format(
+        date=date_str,
+        des=des,
+        to_account=to_acc,
+        to=to_masked,
+        from_type=from_acc,
+        from_acc=from_masked,
+        amount=amount,
+        currency=currency
+    ))
 
 data_from_json = catch_data()
 filtered_data = filter_dates(data_from_json)
